06d8503a3fdcbd8f33fdf762dc6265f528a9f456
angel <angel@DESKTOP-8ELEIVF>
2024-05-30 23:08:10 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Método cuando el texto de la entrada cambia 
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string pasado = e.OldTextValue;    //Valor anterior 
+            string nuevo = e.NewTextValue; //Nuevo valor 
+        }
+
+        //Método cuando se completa la entrada
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+          
+        }
+    }
+
+}
